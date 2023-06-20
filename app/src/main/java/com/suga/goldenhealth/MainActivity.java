8335d8cd2f45db5489b0310de560c78084dce861
bheatleyyy <sugakesshaa@gmail.com>
2023-06-20 23:45:45 +0800

    GoldenHealth: Initial skeleton app

---
+++ app/src/main/java/com/suga/goldenhealth/MainActivity.java
@@ -0,0 +1,14 @@
+package com.suga.goldenhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
